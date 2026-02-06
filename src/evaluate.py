a300de9d9a086ac7cf680230921fb1d61e16540d
jigmeTenzin <jigten2005@gmail.com>
2026-02-06 10:06:06 +0600

    Added file

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,24 @@
-def evaluate_model(model, test_data):
-    # TODO: Generate predictions
-    # TODO: Calculate metrics (accuracy, precision, recall, F1)
-    # TODO: Create confusion matrix
-    # TODO: Save metrics to eval_metrics.json
-    # TODO: Log to MLflow
-    pass
+import json
+import pickle
+import mlflow
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+
+def evaluate_model(preds, labels):
+    accuracy = accuracy_score(labels, preds)
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels, preds, average="binary"
+    )
+
+    metrics = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1
+    }
+
+    with open("eval_metrics.json", "w") as f:
+        json.dump(metrics, f, indent=4)
+
+    mlflow.log_metrics(metrics)
+    return metrics
--- src/model.py
+++ src/model.py
@@ -1,13 +1,66 @@
+import pickle
+import mlflow
+import yaml
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    Trainer,
+    TrainingArguments
+)
+import torch
+
+
 class SentimentModel:
-    def __init__(self, model_name="distilbert-base-uncased"):
-        # TODO: Load tokenizer from HuggingFace
-        # TODO: Load pre-trained model
-        # TODO: Freeze/unfreeze layers as needed
-        pass
-    
-    def train(self, train_data, val_data, epochs=3):
-        # TODO: Create Trainer with training arguments
-        # TODO: Log hyperparameters to MLflow
-        # TODO: Train and validate
-        # TODO: Save model and log to MLflow
-        pass
+    def __init__(self, model_name):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels=2
+        )
+
+    def train(self, train_data, val_data, config):
+        training_args = TrainingArguments(
+            output_dir="models/trained",
+            evaluation_strategy="epoch",
+            learning_rate=config["learning_rate"],
+            per_device_train_batch_size=config["batch_size"],
+            per_device_eval_batch_size=config["batch_size"],
+            num_train_epochs=config["epochs"],
+            logging_dir="./logs",
+            save_strategy="epoch"
+        )
+
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=train_data,
+            eval_dataset=val_data,
+            tokenizer=self.tokenizer
+        )
+
+        mlflow.start_run()
+        mlflow.log_params(config)
+
+        trainer.train()
+        eval_results = trainer.evaluate()
+
+        mlflow.log_metrics(eval_results)
+
+        self.model.save_pretrained("models/trained/model")
+        self.tokenizer.save_pretrained("models/trained/model")
+
+        mlflow.end_run()
+
+
+if __name__ == "__main__":
+    with open("params.yaml") as f:
+        params = yaml.safe_load(f)
+
+    with open("data/processed/train.pkl", "rb") as f:
+        train_data = pickle.load(f)
+
+    with open("data/processed/test.pkl", "rb") as f:
+        test_data = pickle.load(f)
+
+    model = SentimentModel(params["train"]["model_name"])
+    model.train(train_data, test_data, params["train"])
